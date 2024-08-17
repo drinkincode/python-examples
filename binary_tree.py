e6c1300a5ae238e5746d1b31ab317e04a0ae96f9
drinkincode] <jonnysmith696910@gmail.com>
2024-08-16 19:59:43 -0500

    binary tree example

--- binary_tree.py
+++ binary_tree.py
@@ -1,8 +1,42 @@
-class binary_tree():
-    def __init__():
-        return
+
+class TreeNode():
+    def __init__(self, payload, left=None, right=None):
+        self.payload = payload
+        self.left = left
+        self.right = right
+        
+class BinaryTree():
+    def __init__(self, payload):
+        self.root = TreeNode(payload=payload)
+    
+    def insert(self, payload):
+        return self._insert(node=self.root, payload=payload)
+        
+    def _insert(self, node: TreeNode, payload):
+        if payload < node.payload:
+            if not node.left:
+                node.left = TreeNode(payload=payload)
+                return True
+        
+        if not node.right:
+            node.right = TreeNode(payload=payload)
+            return True
+        self._insert(node.right, payload)
+        
+    def print_tree(self):
+        self._print_tree(self.root)
+        
+    def _print_tree(self, node: TreeNode):
+        if node.left:
+            self._print_tree(node.left)
+            
+        print(node.payload)
+        
+        if node.right:
+            self._print_tree(node.right)
         
         
-class node():
-    def __init__():
-        
+tree = BinaryTree(50)
+tree.insert(60)
+tree.insert(40)
+tree.print_tree()
